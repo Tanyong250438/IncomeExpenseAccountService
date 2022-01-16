7a193635bb66695253d6c2cba869d560d561170c
jutarat_k <jutarat_k@mfec.co.th>
2022-01-16 08:49:22 +0700

    initial

---
+++ src/main/java/com/service/IncomeExpenseAccountService/Application.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service.IEAS;
+
+import java.util.Date;
+import java.util.TimeZone;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ *
+ * @author Anuwat_K
+ */
+@SpringBootApplication
+@EnableAsync
+public class Application implements CommandLineRunner {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("|------------ Details ------------");
+        System.out.println();
+        System.out.println("|--- Environment : " + activeProfile);
+        System.out.println();
+        System.out.println("|--- Default Timezone : " + TimeZone.getDefault().getID() + " at Date : " + new Date());
+        System.out.println();
+        System.out.println("|---------------------------------");
+
+    }
+}
---
+++ src/main/java/com/service/IncomeExpenseAccountService/controller/ApplicationController.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service.IEAS.controller;
+
+import com.service.IEAS.constant.CommonConstant;
+import com.service.IEAS.service.LoggerService;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
+import javax.net.ssl.HttpsURLConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Anuwat_K
+ */
+@RestController
+public class ApplicationController {
+
+    @Value("${bulid.enviromment}")
+    private String env;
+
+    @Value("${bulid.name}")
+    private String appName;
+
+    @Value("${bulid.version}")
+    private String appVersion;
+
+    @Value("${bulid.timestamp}")
+    private String appTimestamp;
+
+    @Autowired
+    private LoggerService logger;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String getVersion() throws Exception {
+        Date startDate = new Date();
+        StringBuilder sb = new StringBuilder();
+
+//        String result = rawDatabaseConnection("jdbc:oracle:thin:@172.30.123.7:1521/RESUAT",
+//                "root", "p@ssw0rd", "SELECT * FROM MFEC_USER.PCK_USER_LOGIN");
+
+        sb
+                .append(" Name: ").append(appName).append(",")
+                .append(" Environment: ").append(env).append(",")
+                .append(" Version: ").append(appVersion);
+
+        logger.accessLogger(startDate, new Date(), "Application_Details", "Succeed.", null,
+                sb.toString(), CommonConstant.LOG_LEVEL_INFO);
+        return sb.toString();
+    }
+
+    public static String getStatus(String url) throws Exception {
+
+        String result = "";
+        int code = 200;
+        try {
+            URL siteURL = new URL(url);
+            HttpsURLConnection connection = (HttpsURLConnection) siteURL.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(3000);
+            connection.connect();
+
+            code = connection.getResponseCode();
+            if (code == 200) {
+                result = "-> Green <-\t" + "Code: " + code;
+            } else {
+                result = "-> Yellow <-\t" + "Code: " + code;
+            }
+        } catch (IOException e) {
+            result = "-> Red <-\t" + "Wrong domain - Exception: " + e.getMessage();
+        }
+        System.out.println(url + "\t\tStatus:" + result);
+        return result;
+    }
+
+    private static String rawDatabaseConnection(String databaseUrl, String databaseUsername, String databasePassword,
+            String query) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = DriverManager.getConnection(databaseUrl, databaseUsername, databasePassword)) {
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery(query)) {
+                    int i = 1;
+                    while (rs.next()) {
+                        sb.append("########## Result: " + i + " ##########");
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            }
+
+        } catch (Exception e) {
+            throw e;
+
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            rawDatabaseConnection("jdbc:mysql://localhost:3306/income_expense_account_service",
+                    "root", "P@ssw0rd", "SELECT * FROM pet");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
